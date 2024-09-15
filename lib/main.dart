216dd4cb84f216ec27f72fe0581f57bca95cd695
tahagiremle <tahagiremle@gmail.com>
2024-09-15 21:07:07 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:doctor_booking_flutter/pages/homepage.dart';
 import 'package:doctor_booking_flutter/pages/login.dart';
 import 'package:doctor_booking_flutter/pages/onboarding.dart';
 import 'package:doctor_booking_flutter/pages/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
